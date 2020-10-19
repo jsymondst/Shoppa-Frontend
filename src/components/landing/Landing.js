e10f7f1e2cbecabff32bc60aebfeca1432926482
jammiesymondstayler@gmail.com <jammiesymondstayler@gmail.com>
2020-10-19 17:54:23 +0100

    logged in / out / backend down logic

--- src/components/landing/Landing.js
+++ src/components/landing/Landing.js
@@ -1,27 +1,50 @@
 import React, { useState, useContext, useEffect } from "react";
 import UserContext from "../../context/user.context";
 import Disconnected from "./Disconnected";
+import { fetchGetWithToken, API_URL } from "../../api/api";
 
 import Main from "../main/Main";
 import Welcome from "../welcome/Welcome";
-import { login } from "../../api/api";
 
 const Landing = () => {
     const [loginState, setLoginState] = useState("pending");
-    const { user, updateUser } = useContext(UserContext);
-    const getUsernameFromToken = () => {
-        let token = localStorage.getItem("userToken");
-        if (token) {
-            updateUser(JSON.parse(atob(token.split(".")[1])));
-        }
-        return token ? JSON.parse(atob(token.split(".")[1])).username : null;
-    };
+    const { user, userControls } = useContext(UserContext);
+    // const getUsernameFromToken = () => {
+    //     let token = localStorage.getItem("userToken");
+    //     if (token) {
+    //         updateUser(JSON.parse(atob(token.split(".")[1])));
+    //     }
+    //     return token ? JSON.parse(atob(token.split(".")[1])).username : null;
+    // };
 
     const checkIn = () => {
         let token = localStorage.getItem("userToken");
-
-        alert("test");
+        if (token) {
+            fetchGetWithToken("checkin")
+                .then((res) => res.json())
+                .then((data) => {
+                    console.log(data);
+                    if (data.user) {
+                        userControls.updateUser(data.user.username);
+                        setLoginState("in");
+                    } else {
+                        userControls.updateUser(null);
+                        setLoginState("out");
+                    }
+                });
+        } else {
+            fetch(API_URL).then(() => {
+                userControls.updateUser(null);
+                setLoginState("out");
+            });
+        }
+        // else {
+        //     userControls.updateUser(null);
+        //     setLoginState("out");
+        // }
     };
+
+    useEffect(() => checkIn(), [user]);
 
     const renderLanding = () => {
         switch (loginState) {
