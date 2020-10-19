d7f23c0590d757916cf73382efa3ea6c321bc487
jammiesymondstayler@gmail.com <jammiesymondstayler@gmail.com>
2020-10-19 17:53:29 +0100

    initial loggedin page

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useContext } from "react";
+import { Button, Icon } from "semantic-ui-react";
+import UserContext from "../../context/user.context";
+import { fetchGetWithToken } from "../../api/api";
 
 const Main = () => {
-    return null;
+    const { user, userControls } = useContext(UserContext);
+
+    const listIndex = () => {
+        fetchGetWithToken("lists")
+            .then((res) => res.json())
+            .then(console.log);
+    };
+
+    return (
+        <div>
+            <h1>{`Welcome, ${user}`}</h1>
+            <Button onClick={listIndex}>List Lists</Button>
+            <Button onClick={userControls.handleLogout}>Logout</Button>
+        </div>
+    );
 };
 
 export default Main;
