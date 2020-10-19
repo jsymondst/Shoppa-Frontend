13f820ccb683ef3c4c06de7416d06244750f1eb0
jammiesymondstayler@gmail.com <jammiesymondstayler@gmail.com>
2020-10-19 17:55:09 +0100

    updated login page and logic

--- src/components/welcome/Login.js
+++ src/components/welcome/Login.js
@@ -4,42 +4,42 @@ import { API_URL, login } from "../../api/api";
 import { Form, Input, Button } from "semantic-ui-react";
 
 export const Login = () => {
-  const [password, setPassword] = useState();
-  const [username, setUsername] = useState();
-  const { user, updateUser } = useContext(UserContext);
-  const getUsernameFromToken = () => {
-    let token = localStorage.getItem("userToken");
-    return token ? JSON.parse(atob(token.split(".")[1])).username : null;
-  };
-  useEffect(() => updateUser(getUsernameFromToken), []);
+    const [password, setPassword] = useState();
+    const [username, setUsername] = useState();
+    const { user, userControls } = useContext(UserContext);
+    // const getUsernameFromToken = () => {
+    //     let token = localStorage.getItem("userToken");
+    //     return token ? JSON.parse(atob(token.split(".")[1])).username : null;
+    // };
+    // useEffect(() => updateUser(getUsernameFromToken), []);
 
-  const handleLogin = (e) => {
-    e.preventDefault();
-    // alert(`username: ${username}, password: ${password}`);
-    login(username, password).then((data) => {
-      if (data.user) {
-        updateUser(data.user.username);
-        localStorage.setItem("userToken", data.jwt);
-      }
-    });
-    // updateUser(username);
-  };
-  return (
-    <Form onSubmit={handleLogin}>
-      <Input
-        default="username"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-      />
-      <Input
-        default="password"
-        type="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <Button type="submit">Login</Button>
-    </Form>
-  );
+    const handleLogin = (e) => {
+        e.preventDefault();
+        // alert(`username: ${username}, password: ${password}`);
+        login(username, password).then((data) => {
+            if (data.user) {
+                userControls.updateUser(data.user.username);
+                localStorage.setItem("userToken", data.jwt);
+            }
+        });
+        // updateUser(username);
+    };
+    return (
+        <Form onSubmit={handleLogin}>
+            <Input
+                default="username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+            />
+            <Input
+                default="password"
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+            />
+            <Button type="submit">Login</Button>
+        </Form>
+    );
 };
 
 export default Login;
